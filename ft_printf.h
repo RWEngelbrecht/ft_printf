712441f2f13357e397d60dfd1080529cd8bf17ae
Rigardt Engelbrecht <rengelbr@c2r2s5.wethinkcode.co.za>
2019-08-30 12:46:58 +0200

    print_hex_mem prototype added

--- ft_printf.h
+++ ft_printf.h
@@ -6,17 +6,21 @@
 /*   By: rengelbr <rengelbr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/29 13:56:00 by rengelbr          #+#    #+#             */
-/*   Updated: 2019/08/30 08:46:36 by rengelbr         ###   ########.fr       */
+/*   Updated: 2019/08/30 12:42:49 by rengelbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 #include <stdarg.h>
+#include <string.h>
 
 int		print_char(va_list ap);
 int		print_string(va_list ap);
 int		print_int_var(va_list ap, char * var);
 int		print_octal_var(va_list ap, char * var);
+int		print_hex_mem(va_list ap);
+
+void	print_memory(const void *addr, size_t size);
 
 #endif
