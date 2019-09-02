d77bcd02ab041bb4451bbd8b9a7e5bba84a275b7
Rigardt Engelbrecht <rengelbr@c2r2s5.wethinkcode.co.za>
2019-09-02 14:20:32 +0200

    can now print unsigned octal

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: rengelbr <rengelbr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/10 10:48:24 by rengelbr          #+#    #+#             */
-/*   Updated: 2019/08/30 12:56:15 by rengelbr         ###   ########.fr       */
+/*   Updated: 2019/09/02 14:11:59 by rengelbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,48 +37,6 @@ The format str can consist of 0 or more directives: Can be non-% chars, which is
 The format str can be 1 or multiple conversion specifications, each results in using 0 or more subsequent args. These are introduced with '%' preceding it.
 It writes its output to stdout.
 */
-
-// char	*ft_itoa_base(int value, int base)
-// {
-// 	char	*base_str = "0123456789ABCDEF";
-// 	char	*ret;
-// 	long	num;
-// 	int		len;
-
-// 	len = 0;
-// 	if (value == 0)
-// 		return ("0");
-// 	if (value == 2147483647 && base == 10)
-// 		return ("2147483647");
-// 	if (value == -2147483648 && base == 10)
-// 		return ("-2147483648");
-// 	num = value;
-// 	while (num)
-// 	{
-// 		len++;
-// 		num /= base;
-// 	}
-// 	num = value;
-// 	if (value < 0)
-// 	{
-// 		if (base == 10)
-// 			len++;
-// 		num *= -1;
-// 	}
-// 	ret = (char*)malloc(sizeof(char) * len + 1);
-// 	ret[len] = '\0';
-// 	while (num)
-// 	{
-// 		ret[--len] = base_str[num % base];
-// 		num /= base;
-// 	}
-// 	if (value < 0 && base == 10)
-// 		ret[0] = '-';
-// 	return (ret);
-// }
-
-
-
 
 #include <stdio.h>
 int		ft_printf(const char *format, ...)
@@ -179,21 +137,23 @@ int main()
 	char c1 = 'c';
 //	void *ptr = &c1;
 	unsigned char c2 = 'u';
-	char *str1 = "now print this";
+	char *str1 = "ffffffff";
 	int nbr;
 	unsigned short sh = 32767;
 	//void *ptr;
 
 //	ft_putstr(str);
 	nbr = ft_printf("%c %c, also %s and %s\n", c1, c2, str, str1);
+	ft_printf("ft_mem address == %p\n", (void*)&c1);
 //	ptr = &nbr;
 	ft_printf("\nnbr == %d\n", nbr);
 	ft_printf("hi == %hi\n", sh);
-//	ft_printf("o == %o\n", c1);
-//	printf("o: %o\n", c1);//prints "signed char" but not really. ascii val.
+	//ft_printf("ft_o == %o\n", c1);
+	ft_printf("\n%o\n", c1);
+	printf("ft_atoi_base %d\n", ft_atoi_base("50", 8));
+	printf("u: %u\n", str1);//prints "signed char" but not really. ascii val.
 //	printf("hd: %hd\n", nbr);//prints short
 //	printf("mem address == %p\n", (void*)&c1);
-	ft_printf("ft_mem address == %p\n", (void*)&c1);
 
 	return (0);
 }
